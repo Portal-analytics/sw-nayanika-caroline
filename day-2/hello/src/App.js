dd6ef9fbe19b13a922c8761f5d3d35e34ca7fb46
nayanika <nayanika@nayanikas-Air.lan>
2017-05-25 11:18:07 -0400

    Challenge-2

--- day-2/hello/src/App.js
+++ day-2/hello/src/App.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+var _this;
+
+var names = [ "get up", "stop sleeping", "drink coffee", "go shower", "wake up"]
+  
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      index: 0
+    };
+    //this.changeState = this.changeQuote.bind(_this);
+  }
+
+
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      
+         <h1 onClick ={() => this.changeState()}>{names[this.state.index]}</h1>
+
       </div>
     );
-  }
 }
 
+changeState() {
+  console.log("am i here");
+  if (this.state.index == 4) {
+    this.setState({
+      index: 0
+    });
+  }
+
+
+  else {
+    var index = this.state.index + 1
+    this.setState ( {
+      index: index
+    });
+  }
+}
+}
 export default App;
