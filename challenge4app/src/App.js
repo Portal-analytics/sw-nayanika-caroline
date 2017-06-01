62e45d639e41a1ec83e6ea6dae3cc180db93d5e4
kins3lla <kins3lla@gmail.com>
2017-06-01 13:27:39 -0400

    cleaned up code, still working with index, problem of refreshing with submit, set up firebase but need the handleSubmit to operate correctly in order to text

--- challenge4app/src/App.js
+++ challenge4app/src/App.js
@@ -2,9 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 //import * as edit from 'react-edit';
-
+import * as firebase from 'firebase'
 
   ///
+
+var config = {
+  apiKey: "AIzaSyCfGu22myhTt4-2i0-TuZpoIyuznKuVOFQ",
+  authDomain: "contact-tracker-ccf9c.firebaseapp.com",
+  databaseURL: "https://contact-tracker-ccf9c.firebaseio.com",
+  storageBucket: "contact-tracker-ccf9c.appspot.com",
+};
+firebase.initializeApp(config);
+
+// Get a reference to the database service
+  var database = firebase.database();
+
+var testCase = {
+  name: "nayanika",
+  name2: "caroline",
+}
+
+firebase.database().ref('testCase').set(testCase);
+var readData = firebase.database().ref("newContracts");
 
 class App extends Component {
   constructor(props){
@@ -13,69 +32,42 @@ class App extends Component {
       name:"",
       description:"",
       price:"",
+      index:"",
+      count: 0,
       contracts:[]
     }
     this.handleNameChange=this.handleNameChange.bind(this);
     this.handleDesriptionChange=this.handleDescriptionChange.bind(this);
     this.handlePriceChange=this.handlePriceChange.bind(this);
+    this.handleIndexChange=this.handlePriceChange.bind(this);
     this.handleSubmit=this.handleSubmit.bind(this);
-    this.handleClick = this.handleClick.bind(this);
+    //this.handleClick = this.handleClick.bind(this);
   }
 
   handleNameChange(e){
-    const description=this.state.description;
-    const price=this.state.price;
-    const contract=this.state.contracts;
-    const index=this.state.index;
     this.setState({
-      name:e.target.value,
-      description: description,
-      price: price,
-      contracts:contract,
-      index: index
+      ...this.state,
+      name:e.target.value
     })
     console.log(this.state)
   }
 
   handleDescriptionChange(e){
-    const name=this.state.name;
-    const price=this.state.price;
-    const contract=this.state.contracts;
-    const index=this.state.index;
     this.setState({
-      name:name,
-      description: e.target.value,
-      price: price,
-      contracts:contract,
-      index: index
+      ...this.state,
+      description: e.target.value
     })
     console.log(this.state)
   }
 
   handlePriceChange(e){
-    const description=this.state.description;
-    const contract=this.state.contracts;
-    const name=this.state.name;
-    const index=this.state.index;
     this.setState({
-      name: name,
-      description: description,
-      price: e.target.value,
-      contracts:contract,
-      index: index
+      ...this.state,
+      price: e.target.value
     })
     console.log(this.state)
   }
   
-  /*
-  const Counter = React.createClass({
-    incrementCount: function(){
-    this.setState({
-      count: this.state.count + 1
-    });
-    */
-
-    
 
     handleSubmit(e){
       var infoArray=[];
@@ -83,31 +75,31 @@ class App extends Component {
       infoArray.push(this.state.description);
       infoArray.push(this.state.price);
       infoArray.push(this.state.index); //info array ...retrieve the index by asking infoArray[3]; 
-      let newContracts = this.state.contracts;
-      newContracts.push(infoArray)
-      let newContractsObject = {}; //add to prev contract and then set state
-      //newContractsObject.push(newContracts); 
+      let newContracts = this.state.infoArray;
+      
+      let newContractsObject = {
+        ...this.state
+      }; 
+      
+      newContracts.push(newContractsObject);
+      
+      var count = this.state.count + 1;
+      
+      let index = {
+        if(index = null){
+            index = count;
+        }
+      }
+
       this.setState({
-        name:this.state.name,
-        description: this.state.description,
-        price: this.state.price,
-        index: this.state.index,
-        contracts: newContracts,
-        newContractsObject: this.state.newContractsObject
+        ...this.state.newContracts  
       })
       console.log(this.state) //so here we need to NOT just print it...
       e.preventDefault();
     }
 
+/*
     handleIndexChange(e){
-    
-     var infoArray=[];
-      infoArray.push(this.state.name);
-      infoArray.push(this.state.description);
-      infoArray.push(this.state.price);
-      infoArray.push(this.state.index); //info array ...retrieve the index by asking infoArray[3]; 
-      let newContracts = this.state.contracts;
-      newContracts.push(infoArray)
     //if statement to check if index already assigned; so would keep it when pressed submit. otherwise, increment.
     const count = this.state.count +1; //attach that to onclick for submit //assign index on submit //need index field
     const index = infoArray[3];
@@ -117,26 +109,28 @@ class App extends Component {
     }
     else{
       //this.state.index = count; 
-      infoArray.index = count;
+      //infoArray.index = count;
     }
   }
+  */
   //convert from array of arrays to an array of objects
-    
-    handleClick(index) {
+
+   /* 
+    handleClick() {
     //find the index of the edit button -- listContracts  index 
     //get the index by accessing the ... 
-    this.state.name = this.name;
-    this.state.description = this.index.description;
-    this.setState(prevState => ({
-      isToggleOn: !prevState.isToggleOn
-    }));
+    this.setState({
+      handleSubmit
+    })
   }
+  */
+
 //nested textbox // give value id //then call set state with tthose new values.... 
 
   render() {
-    const listContracts = this.state.contracts.map((c, index) =>
-      <li> { "Name: "+c[0]+" Description: "+c[1]+" Price: "+c[2]} <button onClick={this.handleClick}>edit</button></li>
-    );
+    const listContracts = this.state.contracts.map(c => {
+      <li> { "Name: "+c.name+" Description: "+c.description+" Price: "+c.price+" Index:"+c.index} <button onClick={this.handleSubmit}>edit</button></li>
+    });
     //add index to onclick and store in state  --= y/n editing
 
     console.log(listContracts);
--- challenge4app/src/index.js
+++ challenge4app/src/index.js
@@ -10,15 +10,6 @@ import * as firebase from 'firebase'
 
 // Initialize Firebase
 // TODO: Replace with your project's customized code snippet
-var config = {
-  apiKey: "AIzaSyCfGu22myhTt4-2i0-TuZpoIyuznKuVOFQ",
-  authDomain: "contact-tracker-ccf9c.firebaseapp.com",
-  databaseURL: "https://contact-tracker-ccf9c.firebaseio.com",
-  storageBucket: "contact-tracker-ccf9c.appspot.com",
-};
-firebase.initializeApp(config);
 
-// Get a reference to the database service
-  var database = firebase.database();
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
